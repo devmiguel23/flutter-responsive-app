17ce41cadd3587c947f43f51473964329c10d337
Miguel De Jesus <8982063-mdesarrollador23@users.noreply.gitlab.com>
2023-01-01 14:53:06 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_responsive/responsive/desktop_body.dart';
+import 'package:flutter_app_responsive/responsive/tablet_body.dart';
+import 'responsive/mobile_body.dart';
+import 'responsive/responsive_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: ResponsiveLayout(
+        mobileBody: const MobileScaffold(),
+        tabletBody: const TabletScaffold(),
+        desktopBody: const DesktopScaffold(),
+      ),
+    );
+  }
+}
